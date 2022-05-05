ec2d872b18503cb9d76457a795097270104e4053
sowmya18-av <sowmyaangadiv18@gmail.com>
2022-05-05 13:51:10 +0530

    deliver file

--- scripts/deliver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-
- VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-
- java -jar target/${NAME}-${VERSION}.jar
